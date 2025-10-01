2d5f0ef8b6c19ba04042af9d747b6c93c8883fba
lacevesine <laceves013@student.fuhsd.org>
2025-10-01 15:19:18 +0000

    blah ablhahsdfha

--- game.js
+++ game.js
@@ -14,5 +14,16 @@ function setup() {
 
 function draw(){
   background(51);
-  Image(environment, 0, 0, 300, 300)
+  // image(environment, 0, 0, 300, 300)
+  for(let x = 0; x < environment.width; x++){
+    for(let y = 0; y < environment.height; y++){
+      let cellWidth = winWidth/environment.width
+      let cellHeight = winHeight/environment.height
+      //if(environment.get(x, y)[0] > 0){
+        rect(x * cellWidth, y * cellHeight, cellWidth, cellHeight)
+      //}
+      print()
+    }
+  }
+  
 }
